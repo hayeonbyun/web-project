dd4d15e5bc885042b3d5ee8ac3422332a175b601
ha <hbyunart@gmail.com>
2026-02-27 15:18:27 +0000

    Make Task model user nullable and remove login requirement

--- todo_function/views.py
+++ todo_function/views.py
@@ -5,13 +5,12 @@ from .models import Task
 from .forms import TaskForm
 
 def task_list(request):
-    tasks = Task.objects.filter(user=request.user)
+    tasks = Task.objects.all()
 
     if request.method == "POST":
         form = TaskForm(request.POST)
         if form.is_valid():
             task = form.save(commit=False)
-            task.user = request.user
             task.save()
             return redirect('task_list')
     else:
